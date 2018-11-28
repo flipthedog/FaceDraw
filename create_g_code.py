bd8b92182212bf3904304cb4dce420350921167c
flipthedog <florisvanrossum@gmail.com>
2018-11-28 21:51:05 +0900

    Working on line segmenting function

--- create_g_code.py
+++ create_g_code.py
@@ -4,17 +4,125 @@
 import sys
 import sysconfig
 import time
+import math
+
+# Image processing imports
 import process_image
-import cv2 as cv
+import numpy as np
+from numpy import sum
 import _datetime
 import cv2 as cv
 
-test_image = process_image.openImage('slicer_test_1.png')
-cv.imshow('test', test_image)
-cv.waitKey(0)
-cv.destroyAllWindows()
 
+
+# containsPixels()
+# Look through the image to see if there are still lines to be found
+# Input: check_image: A black and white image to check
+# Output: Boolean value representing whether the image still contains pixels
+def containsPixels(check_image):
+    shape = check_image.shape
+    width = shape[0]
+    height = shape[1]
+    # print("X: " + str(width) + " Y: " + str(height))
+
+    zeros = np.zeros((width,height), dtype=np.uint8)
+    ones = np.ones((width,height), dtype=np.uint8)
+    ones = ones * 255
+
+    indices = np.where( check_image == [255])
+    coordinates = [indices[0],indices[1]]
+
+    new_coordinates = np.asarray(coordinates,dtype=np.uint8)
+
+    print(len(new_coordinates[0]))
+    if not len(new_coordinates[0]) > 0:
+        return False
+    else:
+        return True
+
+    # For testing and printing all pixels in the image
+    # i = 0
+    # j = 0
+    #
+    # while i in range(0, width - 1):
+    #     j = 0
+    #     i = i + 1
+    #     while j in range(0, height - 1):
+    #         j = j + 1
+    #         print(str(check_image.item(i, j, 2)))
+
+# distanceBetween()
+# Find the distance between two pixels
+def distanceBetween(pixel_1, pixel_2):
+
+    return math.sqrt((pixel_1[0] - pixel_2[0])^2 + (pixel_1[1] - pixel_2[1])^2)
+
+
+# findNear()
+# Find nearby black pixels to take
+# Input: Image to check for nearby pixels
+# x, y coordinate of the pixel
+
+def findNear(check_image, x, y):
+
+    home_pixel = [x, y]
+
+    white_pixels = cv.findNonZero(cv.bitwise_not(check_image))
+
+    results = []
+    points = []
+    distance = []
+
+    for i in range(0, len(white_pixels)):
+
+        distance = distanceBetween(white_pixels[i], home_pixel)
+        print(distance)
+
+        points.append(white_pixels[i])
+        distance.append(distance)
+
+# readLines()
+# Find the lines in the image and pass them back as an array
+# Input: test_image: A black and white image representing the image to be drawn
+# Output: An array of lines to pass through the slicer function
 def readlines(test_image):
+    worked_image = test_image
+
+    shape = test_image.shape
+    width = shape[0]
+    height = shape[1]
+
+    i = 0
+    j = 0
+
+    while i in range(0, width - 1):
+        j = 0
+        i += 1
+        while j in range(0, height - 1):
+
+
+            pixel = worked_image[i, j]
+
+            print(pixel)
+
+            if pixel is 0:
+                print("Got one")
+
+                worked_image = findNear(worked_image, pixel)
+
+                # This means we have a black pixel
+                # Search for nearby black pixels
+            else:
+                # This means we have a white pixel, so do nothing
+                None
+
+
+            j += 1
+
+    # An array
+    lines = []
+
+    return containsPixels(worked_image)
 
 
 # image_to_gcode
@@ -94,3 +202,20 @@ def writeConclusion(file):
     # TODO write the conclusion of this
     #
     None
+
+
+test_image = process_image.openImage('slicer_test_1.png')
+black_image = np.zeros((test_image.shape[0],test_image.shape[1]), dtype=np.uint8)
+blank_image = np.ones((test_image.shape[0],test_image.shape[1]), dtype=np.uint8) * 255
+
+
+run_image = test_image
+
+print(str(readlines(run_image)))
+
+cv.imshow('original', run_image)
+# cv.imshow('final', final_image)
+cv.waitKey(0)
+cv.destroyAllWindows()
+
+
